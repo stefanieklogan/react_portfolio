236499448e6ef204c31ec6d76106faddbf81a08e
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-05-25 20:19:14 -0400

    fix photo src

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -11,7 +11,7 @@ function About() {
                     <div class="col-12 text-center">
                         <img class="allimages mx-2 my-1" alt="Stefanie whisper" src="https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/Stefanie_left.jpg?raw=true"/>
                             <img class="allimages mx-2 my-1" alt="Stefanie surprise" src="https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/stefanie_center.jpg?raw=true"/>
-                                <img class="allimages mx-2 my-1" alt="Stefanie whisper" src="https://github.com/stefanieklogan/react_portfolio/blob/master/src/images/stefanie_right.jpg?raw=true"/>
+                                <img class="allimages mx-2 my-1" alt="Stefanie whisper" src="https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/stefanie_right.jpg?raw=true"/>
           </div>
       </div>
     
--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -4,42 +4,42 @@ import PortfolioCard from "../PortfolioCard";
 
 const portfolioData = [
     {
-        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/src/images/park.jpg?raw=true",
+        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/park.jpg?raw=true",
         alt: "national park",
         href: "https://guie01.github.io/Project-W7-Group-5/",
         description: "Start planning your next outdoor adventure with us!",
         title: "National Parks Adventure"
     },
     {
-        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/src/images/directory.jpg?raw=true",
+        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/directory.jpg?raw=true",
         alt: "employees",
         href: "https://stefanieklogan.github.io/react_employee_directory/",
         description: "Employee management and contact information",
         title: "React Employee Directory"
     },
      {
-        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/src/images/fitness.png?raw=true",
+        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/fitness.png?raw=true",
         alt: "heart rate",
         href: "https://blooming-chamber-74383.herokuapp.com/",
         description: "Keep your body working until you go to sleep",
         title: "Fitness Tracker"
     },
     {
-        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/src/images/dayplanner.JPG?raw=true",
+        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/dayplanner.JPG?raw=true",
         alt: "day planner",
         href: "https://stefanieklogan.github.io/Homework5DayPlanner/",
         description: "Keep track of your day",
         title: "Day Planner"
     },
     {
-        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/src/images/crypto.JPG?raw=true",
+        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/crypto.JPG?raw=true",
         alt: "crypto coins",
         href: "https://radiant-ocean-90430.herokuapp.com/",
         description: "Calling all crypto currency fans!",
         title: "Crypto Chat"
     },
     {
-        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/src/images/money.jpg?raw=true",
+        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/money.jpg?raw=true",
         alt: "cash money",
         href: "https://pure-peak-80351.herokuapp.com/",
         description: "Finances at your fingertips",
@@ -55,7 +55,7 @@ function Portfolio() {
                 <div className="row">
                     {portfolioData.map((item) => (
                         <div className="card-body">
-                            <div className="col-6">
+                            <div className="col-4">
 
                                 <PortfolioCard project={item} />
                             </div>
