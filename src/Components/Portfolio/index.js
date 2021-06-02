89c75bfd28554f2d734238011e9d9c9134dcd71e
Stefanie L <75275831+stefanieklogan@users.noreply.github.com>
2021-06-01 20:12:21 -0400

    add prj 3

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -18,11 +18,11 @@ const portfolioData = [
         title: "React Employee Directory"
     },
      {
-        thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/fitness.png?raw=true",
-        alt: "heart rate",
-        href: "https://blooming-chamber-74383.herokuapp.com/",
-        description: "Keep your body working until you go to sleep",
-        title: "Fitness Tracker"
+        thumbnail: "https://github.com/stefanieklogan/brewery/blob/main/client/src/assets/Celebration_Logo.png?raw=true",
+        alt: "Celebration Brewing",
+        href: "https://pure-journey-55067.herokuapp.com/",
+        description: "Celebration's first brewery!",
+        title: "Celebration Brewing"
     },
     {
         thumbnail: "https://github.com/stefanieklogan/react_portfolio/blob/master/public/images/dayplanner.JPG?raw=true",
